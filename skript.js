bb7d573277bea0208c1b3605439bc13d019a3dd3
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-11-25 14:55:55 +0100

    Update skript.js

--- skript.js
+++ skript.js
@@ -2,7 +2,7 @@ $("button").click(function() {
     $.ajax('https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php', {
         type: 'GET',  // http method
         data: { aid: 1, hash: 123456789 },  // data to submit
-        dataType: 'jsonp',
+        dataType: 'json',
         success: function (data, status) {
             $('p').append('status: ' + status + ', data: ' + data);
         },
