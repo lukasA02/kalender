2ad31b73effe07811d693763b95329ab4bb9b8c1
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-11-25 14:00:14 +0100

    Add files via upload

---
+++ kalender.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalender</title>
+    <link rel="stylesheet" href="stil.css">
+</head>
+<body>
+<button id="tryck">😈😈😈</button>
+
+<div class="month">      
+  <ul>
+    <li class="prev">&#10094;</li>
+    <li class="next">&#10095;</li>
+    <li>
+      August<br>
+      <span style="font-size:18px">2021</span>
+    </li>
+  </ul>
+</div>
+
+<ul class="weekdays">
+  <li>Mo</li>
+  <li>Tu</li>
+  <li>We</li>
+  <li>Th</li>
+  <li>Fr</li>
+  <li>Sa</li>
+  <li>Su</li>
+</ul>
+
+<ul class="days">  
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+  <li>5</li>
+  <li>6</li>
+  <li>7</li>
+  <li>8</li>
+  <li>9</li>
+  <li><span class="active">10</span></li>
+  <li>11</li>
+  <li>12</li>
+  <li>13</li>
+  <li>14</li>
+  <li>15</li>
+  <li>16</li>
+  <li>17</li>
+  <li>18</li>
+  <li>19</li>
+  <li>20</li>
+  <li>21</li>
+  <li>22</li>
+  <li>23</li>
+  <li>24</li>
+  <li>25</li>
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li>30</li>
+  <li>31</li>
+</ul>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.js"
+  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+  crossorigin="anonymous"></script>
+<script src="skript.js"></script>
+</body>
+</html>
---
+++ skript.js
@@ -0,0 +1,12 @@
+$("button").click(function(){
+    $.ajax('https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php', {
+        type: 'GET',  // http method
+        data: { aid: 1, hash: 123456789 },  // data to submit
+        success: function (data, status) {
+            $('p').append('status: ' + status + ', data: ' + data);
+        },
+        error: function (errorMessage) {
+                $('p').append('Error' + errorMessage);
+        }
+    });
+  });
