c390b23ee32d9e23282033bb755a5e72905ccf9a
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-12-02 08:32:06 +0100

    Add files via upload

--- skript.js
+++ skript.js
@@ -4,21 +4,25 @@ $("button").click(function() {
         // data: { aid: 1, hash: 123456789 },  // data to submit
         dataType: 'json',
         success: function (data) {
+            var dagar = Array();
+
             for(let i = 0; i < data.length; i++) {
                 $('p').append(data[i].ID);
                 $('p').append(data[i].Namn);
                 $('p').append(data[i].beskrivning);
                 $('p').append(data[i].Agare);
                 $('p').append(data[i].Starttid);
+                var dag = new Date(data[i].Starttid);
+                dagar[i] = dag.getUTCDate();
                 $('p').append(data[i].Sluttid);
                 $('p').append("<br>");
             }
+            
             var datum = new Date(data[0].Starttid);
-            console.log(datum.toUTCString());
             console.log(datum.getUTCDate());
-            $("#ett").innerHTML = "<span class='active'>" + $("#ett").innerHTML + "</span>";
-            // $("#ett").toggleClass("active");
-            // $('p').append('data: ' + data);
+            var bruh = "<span class='active'>" + $("#ett").html() + "</span>";
+            $("#ett").html(bruh);
+            console.log(dagar);
         },
         error: function (errorMessage) {
             $('p').append('Error' + JSON.stringify(errorMessage));
