7705a6f393d531a43ba5247986f848824098a63b
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-12-02 13:18:17 +0100

    Delete minkalender.php

--- minkalender.php
+++
@@ -1,5 +0,0 @@
-<?php
-//echo file_get_contents("https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php?aid=1&hash=123456789");
-$content = file_get_contents("https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php?aid=1&hash=334595127");
-echo $content;
-?>
