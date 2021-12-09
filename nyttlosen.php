9ec1c3c073f398a91c6336f22e7bcde6b96a0947
lukasA02 <90241603+lukasA02@users.noreply.github.com>
2021-12-09 08:39:55 +0100

    Add files via upload

---
+++ nyttlosen.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div id="nytt">
+        <form action="allterstal.php" method="GET">
+        <div class="box">
+            <h1>Tabort event 😀</h1>
+        </div>
+        <div class="box">
+        <label for="losenTxt">skriv in ditt nya lösenord:</label>
+         <input type="text" name="losenTxt" >   
+        </div>
+        <div id="submit" class="box">
+            <input type="submit" name="submit" value="nytt">
+        </div>
+    </form>
+
+</div>
+</body>
+</html>
