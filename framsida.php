ce3f9addd396ee491401b31b6562ce6eaeee6ab7
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-12-16 11:03:29 +0100

    Update framsida.php

--- framsida.php
+++ framsida.php
@@ -5,24 +5,24 @@ if(isset($_SESSION['aid'], $_SESSION['hash'])) {
     $aid = $_SESSION['aid'];
     $hash = $_SESSION['hash'];
 
-$url= 'https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php?aid='. $aid .'&hash='.$hash;
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$data = curl_exec($ch);
+    $url= 'https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php?aid='. $aid .'&hash='.$hash;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = curl_exec($ch);
 
-$data = json_decode($data);
-$_SESSION['data'] = $data;
+    $data = json_decode($data);
+    $_SESSION['data'] = $data;
 
-$url= 'https://tp2021.ntigskovde.se/Theprovider-main/visaanvandare.php?aid='. $aid .'&hash='.$hash;
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$dataa = curl_exec($ch);
+    $url= 'https://tp2021.ntigskovde.se/Theprovider-main/visaanvandare.php?aid='. $aid .'&hash='.$hash;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $dataa = curl_exec($ch);
 
-$dataa = json_decode($dataa);
-$_SESSION['dataa'] = $dataa;
-//  print_r($data);
+    $dataa = json_decode($dataa);
+    $_SESSION['dataa'] = $dataa;
+    //  print_r($data);
 }
 ?>
 <!DOCTYPE html>
