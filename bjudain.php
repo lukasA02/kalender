00c4545cf088e34d6dcb268e1f4759d6b81f1c7c
lukasA02 <90241603+lukasA02@users.noreply.github.com>
2021-12-16 09:33:23 +0100

    Add files via upload

--- bjudain.php
+++ bjudain.php
@@ -3,10 +3,10 @@ session_start();
 $aid = $_SESSION['aid'];
 $hash = $_SESSION['hash'];
 
-if(isset($_GET["EventID"]) && isset($_GET["anvid"])){
+if(isset($_GET['anvandarid']) && isset($_GET['EventID'])){
 
     $envid = $_GET['EventID'];
-    $anvid = $_GET['anvid'];
+    $anvid = $_GET['anvandarid'];
 
 $url = 'http://tp2021.ntigskovde.se/Theprovider-main/bjudain.php?aid='.$aid.'&hash='.$hash.'&eventid='.$envid. '&anvandarid='.$anvid;
 $ch = curl_init();
