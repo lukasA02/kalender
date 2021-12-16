746c7f937bea159022d426dc0b2faad43b69c9d1
lukasA02 <90241603+lukasA02@users.noreply.github.com>
2021-12-16 09:34:07 +0100

    Add files via upload

--- visaanvandare.php
+++ visaanvandare.php
@@ -61,18 +61,3 @@
 </body>
 </html>
 
-<?php
-session_start();
-$aid = $_SESSION['aid'];
-$hash = $_SESSION['hash'];
-
-$url= 'https://tp2021.ntigskovde.se/Theprovider-main/visaanvandare.php?aid='. $aid .'&hash='.$hash;
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $url);
-$data = curl_exec($ch);
-
-$data = json_decode($data);
-$_SESSION['data'] = $data;
-
-?>
