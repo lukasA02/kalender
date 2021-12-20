818f3ad274586cdbf1d384a07fe2288ead952c94
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2021-12-20 09:31:47 +0100

    Add files via upload

--- redigeraanvandare.php
+++ redigeraanvandare.php
@@ -26,16 +26,13 @@ $data = $_SESSION['dataa'];
             <div>
                 <label for="anvandarid">AnvändarID:</label>
                 <select name="anvandarid">
-                     <?php
-                      //lista alla anvandare och anvandarid, att få upp på den dropdown 
+                    <?php
+                    //lista alla anvandare och anvandarid, att få upp på den dropdown 
                     foreach ($data as $row ) {
-                      //  print_r($row);
                         echo "<option value=". $row->AnvandarID."> ". $row->AnvandarID. ", ".$row->Anvnamn. "</option>";
-                        //print_r($row->AnvandarID);
-                     }
-                     print_r($data);
-                       ?>
-                      </select>
+                    }
+                    ?>
+                </select>
             </div>
             <div>
                 <label for="Behorighet">Behörighet:</label>
