b4ea0da3c4d9f232ac3eee2b1501d77f7d35a9ca
johanoscarsson0 <92022403+johanoscarsson0@users.noreply.github.com>
2022-01-10 10:53:52 +0100

    Add files via upload

--- events.php
+++ events.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
 
-if(isset($_SESSION["aid"], $_SESSION["hash"])) {
+if(isset($_SESSION["aid"], $_SESSION["hash"]) && $_SESSION['behorighet'] == 1) {
     $arr = array("aid" => $_SESSION["aid"], "hash" => $_SESSION["hash"]);
     $json = file_get_contents("https://tp2021.ntigskovde.se/Theprovider-main/minkalender.php?aid=" . $_SESSION["aid"] . "&hash=" . $_SESSION["hash"]);
-    // echo json_encode($arr);
+    echo $json;
+}
+if(isset($_SESSION["aid"], $_SESSION["hash"]) && $_SESSION['behorighet'] == 3) {
+    $arr = array("aid" => $_SESSION["aid"], "hash" => $_SESSION["hash"]);
+    $json = file_get_contents("https://tp2021.ntigskovde.se/Theprovider-main/visarn.php?aid=" . $_SESSION["aid"] . "&hash=" . $_SESSION["hash"]);
     echo $json;
 }
